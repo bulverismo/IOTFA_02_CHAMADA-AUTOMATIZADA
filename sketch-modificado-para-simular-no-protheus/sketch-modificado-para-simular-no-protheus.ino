27fa7d1c094501996e87fb615be93f96f5f68dd3
bulverismo <bulverismo@gmail.com>
2018-11-01 10:46:45 -0300

    save geral

--- sketch-modificado-para-simular-no-protheus/sketch-modificado-para-simular-no-protheus.ino
+++ sketch-modificado-para-simular-no-protheus/sketch-modificado-para-simular-no-protheus.ino
@@ -1,23 +1,9 @@
 /*
-  SD card file dump
-
- This example shows how to read a file from the SD card using the
- SD library and send it over the serial port.
-
- The circuit:
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
-
- created  22 December 2010
- by Limor Fried
- modified 9 Apr 2012
- by Tom Igoe
-
- his example code is in the public domain.
-
+ *
+ *
+ * Funcoes modificadas e codigo sendo recriado do zero
+ *
+ *
  */
 
 #include <SPI.h>
@@ -25,17 +11,17 @@
 
 const int chipSelect = 10;
 int posicao = 1;
+int totalAlunosNaLista = 0;
+uint8_t botaoAcima = 5;
+uint8_t botaoAbaixo = 6;
+uint8_t botaoSeleciona = 4;
+
+boolean selecionar = false;
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-
   Serial.print("Initializing SD card...");
-
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
@@ -47,25 +33,90 @@ void setup() {
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
 
+   totalAlunosNaLista = contarAlunos();
+
+
+   // Seta os botoes
+
+   pinMode(botaoAcima , INPUT);
+   pinMode(botaoAbaixo , INPUT);
+   pinMode(botaoSeleciona , INPUT);
+
+  Serial.print("----->>> DEBUG :  ");
+  Serial.println(totalAlunosNaLista);
 
 }
 
 void loop() {
+
 uint8_t i = exibirAtualSelecionado(posicao);
 Serial.print(" ");
 Serial.println(i);
-posicao++;
+   while(1){
+    
+    
+        if(digitalRead(botaoAcima) == HIGH){
+          if(posicao == totalAlunosNaLista){
+            posicao = 1;
+            break;
+          }else{
+            posicao++;
+            break;
+          }
+          }else if(digitalRead(botaoAbaixo) == HIGH){
+            if(posicao == 1){
+              posicao = totalAlunosNaLista;
+              break;
+            }else{
+              posicao--;
+              break;
+            }
+          }else if(digitalRead(botaoSeleciona) == HIGH){
+            selecionar = true;
+            break;
+          }
+        
+    }
+    
+    if(selecionar){
+      // bla
+      Serial.print("aluno ");
+      exibirAtualSelecionado(posicao);
+      Serial.println(" foi selecionado");
+      Serial.print("Numero de IDS cadastradas ");
+      Serial.println(calcIdParaCadastro());
+      //deseleciona
+      selecionar = false;
+    }
+    
+    delay(180);
+
+}
+
+int calcIdParaCadastro(){
+  int contador = 1;
+  int totalIds = 0;
+  while(1){
+    if(retornaIdAtualSelecionado(contador)!=0){
+      totalIds++;
+    }
+    if (contador == totalAlunosNaLista){
+      return totalIds;
+    }
+    contador++;
+    Serial.print('.');
+  }
 }
 
 
-void contarAlunos(){
+int contarAlunos()
+{
   File dataFile = SD.open("lista.txt");
-
+  int indexAtual = 0;
 // Parte da função que conta quantos alunos tem na lista
 
   if (dataFile) {
     char c;
-    int indexAtual = 0;
     while (dataFile.available()) {
       c = dataFile.read();
       if(c == '\r'){
@@ -73,8 +124,8 @@ void contarAlunos(){
       }
     }
   }
-
   dataFile.close();  // fecha o arquivo de texto que havia sido aberto
+  return indexAtual;
 }
 
 uint8_t exibirAtualSelecionado(int indexEscolhido)
@@ -99,6 +150,58 @@ uint8_t exibirAtualSelecionado(int indexEscolhido)
         // o nome sem armazer esse nome em lugar nenhum
         if(indexAtual == indexEscolhido-1 && c != '#' && !isDigit(c)){
           Serial.print(c);
+        }
+      }
+    }
+
+    // Essa parte da função serve para encontrar o ID que esta gravado dentro
+    // do leitor biometrico
+
+    dataFile.seek(dataFile.position()-5);  // reposiciona o cursor para poder
+                                           // pegar o id associado a este nome
+                                           // ou ver que nao tem nome associado
+                                           // a este id
+
+   // pega o id e poe em um vetor de char
+
+    char tmpChar[3];
+    int cont = 0;
+    while(dataFile.available()){
+      char c = dataFile.read();
+      if(isDigit(c)){
+        tmpChar[cont] = c;
+        cont++;
+      }else if(c == '\r'){
+        tmpChar[cont] = '\0';
+        break;
+      }
+
+    }
+    // parte da conversao em do vetor de char,
+    // vai retorna 0 caso nao tenha id associado ao nome
+    // tem que converte para uint8_t para poder usar na função
+    // da leitora biometrica
+    uint8_t convertido = atoi(tmpChar);
+
+    dataFile.close();  // fecha o arquivo de texto que havia sido aberto
+
+    return convertido; // retorna id ja no formato uint8_t
+}
+
+uint8_t retornaIdAtualSelecionado(int indexEscolhido)
+{
+
+// Função que printa o aluno que esta no index selecionado
+
+  File dataFile = SD.open("lista.txt");
+  int indexAtual = 0;
+
+  while (dataFile.available()) {
+      char c = dataFile.read();
+      if(c == '\r'){
+        indexAtual++;
+        if(indexAtual == indexEscolhido){
+          break;
         }
       }
     }
